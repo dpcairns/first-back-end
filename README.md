7322e6e9676c61c28f000b6428082dc66575744e
harriet-the-spy <dpcairns@gmail.com>
2020-08-11 13:43:12 -0700

    add call to location iq

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# first-back-end
+# City Explorer Backend APIs
+
+## Weatherbit API
+
+https://api.weatherbit.io/v2.0/forecast/daily?&lat=38.123&lon=-78.543&key={api-key}
+
+## Location IQ API
+
+https://us1.locationiq.com/v1/search.php?key={api-key}&q={city-name}&format=json
+
+## Yelp API
+
+https://api.yelp.com/v3/businesses/search?latitude={lat}&longitude={lng}
+
+API key provided in header `Authorization` with value of `Bearer <api-key>`
+
+Also see https://www.yelp.com/developers/display_requirements
+
+## Hiking Project API
+
+https://www.hikingproject.com/data/get-trails?lat={lat}&lon={lng}&maxDistance=200&key={api-key}
+
+## Points Break Down
+
+Looking For | Points (10)
+:--|--:
+Working location endpoint | 4
+Working weather endpoint | 3
+Working trails endpoint | 3
+Working reviews endpoint | +1
+Modularizing and testing your routes | +1 per route
--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const geoData = require('./data/geo.js');
 const weatherData = require('./data/weather.js');
 const request = require('superagent');
 const app = express();
@@ -9,10 +8,18 @@ app.use(cors());
 
 app.use(express.static('public'));
 
-function getLatLong(cityName) {
-    // TODO: an api call--for now it's hard coded
-    const city = geoData[0];
+const { GEOCODE_API_KEY } = process.env;
 
+async function getLatLong(cityName) {
+    // 1) replace hard coded data with a call to the API
+    // 2) add the GEOCODE_API_KEY to .env and pop it in the url
+    // 3) add the city name from the query params
+    const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`);
+
+    // 4) go find the first city in the response
+    const city = response.body[0];
+
+    // 5) change its shape to fit the contract with the front end
     return {
         formatted_query: city.display_name,
         latitude: city.lat,
@@ -20,11 +27,14 @@ function getLatLong(cityName) {
     };
 }
 
-app.get('/location', (req, res) => {
+// 7) therefore, we need to make this route async
+app.get('/location', async(req, res) => {
     try {
         const userInput = req.query.search;
-    
-        const mungedData = getLatLong(userInput);
+
+        // 6) now that getLatLng is asynchronous, we must AWAIT it
+        const mungedData = await getLatLong(userInput);
+
         res.json(mungedData);
     } catch (e) {
         res.status(500).json({ error: e.message });
@@ -35,7 +45,6 @@ app.get('/location', (req, res) => {
 app.get('/chars', async(req, res) => {
     try {
         const response = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=char');
-
         const pokemon = response.body.results;
 
         const names = pokemon.map((poke) => {
